500f57d3b590383e0424bf7a33c8ccccab26910f
ebuaa <axel.aube@viacesi.fr>
2026-04-22 10:33:51 +0200

    ignore next.config.gs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
-    ignores: ["cypress/**/*", "**/*.cy.*", ".next/**/*", "node_modules/**/*"],
+    ignores: ["cypress/**/*", "**/*.cy.*", ".next/**/*", "node_modules/**/*", "next.config.js"],
   },
 ];
